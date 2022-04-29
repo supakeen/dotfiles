3fda870f06a86c5e53dde0fe3ea717f425d3fb4b
Simon de Vlieger <cmdr@supakeen.com>
2022-04-29 14:56:31 -0400

    chore: add an install script

---
+++ install.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euxo pipefail
+
+[[ -e "~/.vimrc" ]] || rm -fr ~/.vimrc
+ln -s `realpath ./vim/vimrc` ~/.vimrc
+
+[[ -e "~/.tmux.conf" ]] || rm -fr ~/.tmux.conf
+ln -s `realpath ./tmux/tmux.conf` ~/.tmux.conf
+
+[[ -e "~/.zshrc" ]] || rm -fr ~/.zshrc
+ln -s `realpath ./zsh/zshrc` ~/.zshrc
+
+[[ -e "~/.gitconfig" ]] || rm -fr ~/.gitconfig
+ln -s `realpath ./git/gitconfig` ~/.gitconfig
