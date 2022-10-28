bd02cab5f6db7803def17f9c81f0bfc6681273f8
Simon de Vlieger <cmdr@supakeen.com>
2022-10-28 21:00:42 +0200

    copy over common-scripts and fix install

--- install.bash
+++ install.bash
@@ -3,6 +3,9 @@ set -euxo pipefail
 
 touch ~/.hushlogin
 
+mkdir -p ~/bin
+cp common-scripts/bin/* ~/bin
+
 [[ -e "~/.vimrc" ]] || rm -fr ~/.vimrc
 ln -s `realpath ./vim/vimrc` ~/.vimrc
 
@@ -21,9 +24,8 @@ ln -s `realpath ./x/Xmodmap` ~/.Xmodmap
 [[ -e "~/.xinitrc" ]] || rm -fr ~/.xinitrc
 ln -s `realpath ./x/xinitrc` ~/.xinitrc
 
-[[ -f "~/.config/alacritty.yml" ]] && rm -fr ~/.config/alacritty.yml
+[[ -e "~/.config/alacritty.yml" ]] || rm -fr ~/.config/alacritty.yml
 ln -s `realpath ./alacritty/alacritty.yml` ~/.config/alacritty.yml
 
-[[ -f "~/.config/xfce4/xfconf" ]] && rm -fr ~/.config/xfce4/xfconf
+[[ -e "~/.config/xfce4/xfconf" ]] || rm -fr ~/.config/xfce4/xfconf
 ln -s `realpath ./xfce4/xfconf` ~/.config/xfce4/xfconf
-
